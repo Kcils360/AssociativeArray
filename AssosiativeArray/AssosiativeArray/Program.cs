a0a6f0de33f56b3a72ab6f89b18be097f3d40a90
Gregory Dukes <kcils360@gmail.com>
2017-10-31 18:32:06 -0700

    initial build, issues with the loops and getting values

---
+++ AssosiativeArray/AssosiativeArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace AssosiativeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] Outside = new string[,]
+            {
+                { "DIA", "Denver Internation Airport" },
+                { "SEA", "Seattle Tacoma Internationl Airport" },
+                { "LAX", "Los Angeles International Airport" }
+            };
+            Console.WriteLine("Please type a key");
+            string get = Console.ReadLine().ToString();
+
+            Search Search = new Search();
+            Search.ArraySearch(Outside, get);
+
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
---
+++ AssosiativeArray/AssosiativeArray/Search.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssosiativeArray
+{
+    class Search
+    {
+        public void ArraySearch(string[,] arr, string val)
+        {
+            for(var i = 0; i < arr.Length; i++)
+            {
+                for(var j = 0; j < 2; j++)
+                {
+                    if(arr[i,j].Contains(val))
+                    {
+                        Console.WriteLine(i[j]);
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Key not found");
+                    }
+                }
+                
+            }
+        }
+    }
+}
